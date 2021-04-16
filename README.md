0fae646acca30d1c1ff2a2f2bb424876e7662d56
moemengaballa@gmail.com <moemengaballa@gmail.com>
2021-04-16 09:27:20 +0200

    first version realtime use pusher

---
+++ README.md
@@ -0,0 +1,49 @@
+## Laravel 8 - realtime comments use laravel echo - pusher 
+
+## Summary
+
+# Realtime comments Laravel 8 /laravel-echo - pusher - vue
+    Add Comment realtime Laravel 8 with Pusher(https://www.youtube.com/playlist?list=PLwAKR305CRO9rlj-U9oOi4m2sQaWN6XA8)
+
+> Laravel 8 API that uses the API resources with a Vue.js frontend
+
+## Quick Start
+
+``` bash
+# Install Dependencies
+composer install
+
+# Run Migrations
+php artisan migrate Or upload DB-file 
+
+# Import Articles
+php artisan db:seed
+
+# Add virtual host if using Apache
+
+# If you get an error about an encryption key
+php artisan key:generate
+
+# Install JS Dependencies
+npm install
+
+# Watch Files
+npm run watch && Copy Same File Style For Page Post
+```
+
+
+### Author
+Edit By Mo'men Gaballa
+
+### Version
+
+1.0.0
+
+### License
+
+This project is licensed under the MIT License
+
+### to Do 
+
+1. use WebSocket - Free 
+
---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\Post;
+use Auth;
+use App\Events\NewComment;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Post $post)
+    {   
+        //dd($post->comments();
+        return response()->json($post->comments()->with('user')->latest()->get());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request,Post $post)
+    {
+        $comment = $post->comments()->create([
+            'body' => $request->body,
+            'user_id' => Auth::id()
+        ]);
+
+        $comment = Comment::where('id', $comment->id)->with('user')->first();
+
+        // event(new NewComment($comment)); // send for all
+        broadcast(new NewComment($comment))->toOthers(); // send it for all with out user add comment
+
+        return $comment->toJson();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comment $comment)
+    {
+        //
+    }
+}
